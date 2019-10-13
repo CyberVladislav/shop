8c1eb61d456c027f6dc1fab91214f9fa1f6d4c5b
Vladislav <foreversmiledface@mail.ru>
2019-10-14 00:13:33 +0300

    Shop-6: Make dynamic comments/reviews and contacts

---
+++ database/seeds/ReviewsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\DB;
+
+use Faker\Factory as Faker;
+class ReviewsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        foreach (range(1,5) as $index) {
+        DB::table('reviews')->insert([ 
+        'user_id'=> $faker->numberBetween(1,3),
+        'rating' => $faker->numberBetween(1,5),
+        'desc' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+        ]);
+        }   
+    }
+}
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -18,6 +18,7 @@
 	<!-- End Banner Area -->
 
 	<!--================Contact Area =================-->
+	@foreach($contacts as $contact)
 	<section class="contact_area section_gap_bottom">
 		<div class="container">
 			<div id="mapBox" class="mapBox" data-lat="40.701083" data-lon="-74.1522848" data-zoom="13" data-info="PO Box CT16122 Collins Street West, Victoria 8007, Australia."
@@ -28,21 +29,22 @@
 					<div class="contact_info">
 						<div class="info_item">
 							<i class="lnr lnr-home"></i>
-							<h6>California, United States</h6>
-							<p>Santa monica bullevard</p>
+							<h6>{{$contact->country}}</h6>
+							<p>{{$contact->street}}</p>
 						</div>
 						<div class="info_item">
 							<i class="lnr lnr-phone-handset"></i>
-							<h6><a href="#">00 (440) 9865 562</a></h6>
-							<p>Mon to Fri 9am to 6 pm</p>
+							<h6><a href="#">{{$contact->phone}}</a></h6>
+							<p>{{$contact->workTime}}</p>
 						</div>
 						<div class="info_item">
 							<i class="lnr lnr-envelope"></i>
-							<h6><a href="#">support@colorlib.com</a></h6>
-							<p>Send us your query anytime!</p>
+							<h6><a href="#">{{$contact->email}}</a></h6>
+							<p>{{$contact->desc}}</p>
 						</div>
 					</div>
 				</div>
+				@endforeach
 				<div class="col-lg-9">
 					<form class="row contact_form" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
 						<div class="col-md-6">
