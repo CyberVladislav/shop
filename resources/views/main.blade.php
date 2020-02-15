ccbd15f93576eeb4a75155b28904f6226f50268c
CyberVladislav <foreversmiledface@mail.ru>
2020-02-15 23:52:01 +0300

    Shop-16: Recode banner slide photo

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -26,9 +26,10 @@
 								</div>
 							</div>
 						</div>
-						<!-- single-slide -->
-						<div class="row single-slide">
-							<div class="col-lg-5">
+
+					<!-- single-slide -->
+					<div class="row single-slide align-items-center d-flex">
+							<div class="col-lg-5 col-md-6">
 								<div class="banner-content">
 									<h1>Nike New <br>Collection!</h1>
 									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
